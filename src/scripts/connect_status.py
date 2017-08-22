42829d92e84db1f7574778d83313bbabe6b06485
João Fonseca <jpedrofonseca_94@hotmail.com>
2017-08-22 02:18:14 +0100

    Refactor connect status; Modified UML Diagram

--- src/scripts/connect_status.py
+++ src/scripts/connect_status.py
@@ -6,76 +6,69 @@ import re
 import sys
 
 
+#Supposing there is a device connected to the raspberry
+#we need to check if the Raspberry can reach it
+#and the quality of the connection(round trip delay)
+
+#In both functions the  information is obtained from a 
+#ping output, to determined network
+
+
 def subprocessping(npings, url):
-    proc = subprocess.Popen(["ping","-c", npings, url],stdout=subprocess.PIPE)
+    proc = subprocess.Popen(["ping","-c", str(npings), url],stdout=subprocess.PIPE)
     proc.wait()
     return proc.communicate()
 
 
-
-##Supposing there is a device connected to the raspberry
-##we need to check if the rasp can reach it
-##and the quality of the connection(round trip delay)
-##
-##In both functions the  information is obtained from a 
-##ping output to determined network
-
-## Average Round Trip Time Delay
+# Average Round Trip Time Delay
 def check_rtt(npings, url):
     (out,err) =subprocessping(npings,url)
 
-    #print out
+    print out
 
     if out.find("packet")==-1:
         print "Cannot reach  " +url
         return -1
 
+    stringlist=out.split("/")
+    rtt=float(stringlist[4]) #avg rtt
 
-    strings1=out.split("/")
-    values1=[]
-
-
-    for i in range(3, 7):
-        values1.append(int(re.search(r'\d+', strings1[i]).group()))
-
-    #Round Trip delay info (ms)
-    if values1[1]>=10:
+    if rtt>=10:
         return 1
     else :
-        if values1[1]<5:
+        if rtt<5:
             return 0
         else:
             return 2
 
 
 
-## Packet loss 
+# Packet loss 
 def check_connection(npings, url):
     (out,err) =subprocessping(npings,url)
 
-    #print out
+    print out
     if out.find("packet")==-1:
         print "Cannot reach  " +url
         return -1
 
-    strings=out.split(",")
-    values=[]
+    stringlist=out.split(",")
+    string=stringlist[2].split() #string[2] ="0% packet loss"
+    packetloss=[int(s) for s in string[0].split() if s.isdigit()]
 
-    for i in range(0, len(strings)):
-        values.append(int(re.search(r'\d+', strings[i]).group()))
-
-    #Percentage of lost packets
-    if values[2]>=50:
+    if packetloss>=50:
         return 1
     else :
-        if values[2]!=0:
+        if packetloss[2]!=0:
             return 0
         else:
             return 2
 
-if __name__ == "__main__":
 
-    var=check_connection("10", "google.pt")
-    var1= check_rtt("10", "google.pt")
-    print "Packet loss: "+str(var)+"\n Round Trip Delay: "+str(var1)
+if __name__ == "__main__":
+    address="google.pt"
+    npings=10
+    print "Packet loss: "+str(check_connection(npings, address))+"\n Round Trip Delay: "+str(check_connection(npings, 
+address))
     sys.exit()
+
